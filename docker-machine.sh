ca6835817937a8c030711e7b27df1714f5dff457
Bertrand Millot <bmillot@20minutes.fr>
2016-08-09 17:50:38 +0200

    Add elasticsearch docker file

--- docker-machine.sh
+++ docker-machine.sh
@@ -6,3 +6,10 @@ docker run --name mysql -e MYSQL_ROOT_PASSWORD=password -d mysql:latest
 docker run --name elasticsearch -d elasticsearch:latest
 docker run --name redis -d redis:latest
 docker run --name apache2 --link mysql:mysql --link elasticsearch:elasticsearch --link redis:redis -d -p 80:80 bmillot/apache2
+docker run --name elasticsearch-01a -d -p 9200:9200 bmillot/elasticsearch /usr/share/elasticsearch/bin/elasticsearch -Des.config=/etc/elasticsearch/elasticsearch.1.yml
+docker run --name elasticsearch-01b --link elasticsearch-01a:node01 -d -p 9201:9200 bmillot/elasticsearch /usr/share/elasticsearch/bin/elasticsearch -Des.config=/etc/elasticsearch/elasticsearch.2.yml
+docker run --name elasticsearch-01c --link elasticsearch-01a:node01 -d -p 9202:9200 bmillot/elasticsearch /usr/share/elasticsearch/bin/elasticsearch -Des.config=/etc/elasticsearch/elasticsearch.3.yml
+
+docker run --name elasticsearch-01a -d -p 9200:9200 bmillot/elasticsearch /usr/share/elasticsearch/bin/elasticsearch -Des.cluster.name=20mn.local.search -Des.node.name=20mn.local.search-01a
+docker run --name elasticsearch-01b --link elasticsearch-01a:node01 -d bmillot/elasticsearch /usr/share/elasticsearch/bin/elasticsearch -Des.cluster.name=20mn.local.search -Des.node.name=20mn.local.search-01b -Des.discovery.zen.ping.unicast.hosts=node01
+docker run --name elasticsearch-01c --link elasticsearch-01a:node01 -d bmillot/elasticsearch /usr/share/elasticsearch/bin/elasticsearch -Des.cluster.name=20mn.local.search -Des.node.name=20mn.local.search-01c -Des.discovery.zen.ping.unicast.hosts=node01
