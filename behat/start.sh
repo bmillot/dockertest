de3d1f925891335c6ccec342f4454bb9e73b5bd6
Bertrand Millot <bmillot@20minutes.fr>
2016-09-02 14:20:11 +0200

    Update Behat image

--- behat/start.sh
+++ behat/start.sh
@@ -13,13 +13,11 @@ if [ -n "$PROFILE" ]; then
 
         # Start Virtual Frame Buffer
         if [ -z "$(ps aux | grep -v grep | grep Xvfb)" ]; then
-            export DISPLAY=:99.0
             Xvfb :99.0 &
         fi
 
         # Start Selenium
         if [ -z "$(ps aux | grep -v grep | grep selenium)" ]; then
-            export _JAVA_OPTIONS="-Xms2048m -Xmx2048m"
             bin/selenium-server-standalone &> /dev/null &
         fi
 
